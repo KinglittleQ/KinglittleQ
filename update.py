cb9acc3dfb345d777152aef7faa6005a6a8473ea
Chengqi Deng <checkdeng0903@gmail.com>
2021-05-09 15:25:19 +0800

    Add table

--- update.py
+++ update.py
@@ -1,9 +1,13 @@
 import sys
+from tabulate import tabulate
 
 
 def update_readme(name):
+    table = [['one','two','three'],['four','five','six'],['seven','eight','nine']]
+    table = str(tabulate(table, tablefmt='html'))
     with open('README.md', 'w', encoding='utf-8') as fp:
-        fp.write(f'issus name: {name}')
+        fp.write(f'issus name: {name}\n\n')
+        fp.write(table)
 
 
 if __name__ == '__main__':
